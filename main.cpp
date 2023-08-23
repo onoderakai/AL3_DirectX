6a12adb25b5cf6fbd2384e024344798a329c2f21
onoderakai <k022g1011@g.neec.ac.jp>
2023-08-24 00:40:14 +0900

    タイトルバーを変更

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow(L"GC2A_07_オノデラ_カイ_AL3");
+	win->CreateGameWindow(L"SpeedShooter");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
